899f473e52128e95c1fd304f5ced47052e345c41
trrussell3 <trrussell3@alaska.edu>
2021-11-08 15:33:02 -0900

    Moved files to test directory and bin directory and refactored print_timing test

---
+++ EPR/Library/tests/timing.h
@@ -0,0 +1,12 @@
+/* File: timing.h */
+
+#ifndef _TIMING_H
+
+#define _TIMING_H
+
+void compute_time(unsigned long, int);
+unsigned long get_time(unsigned long);
+double get_freq(void);
+void print_timing(char *, double, char *);
+
+#endif
