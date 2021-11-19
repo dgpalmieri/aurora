10661d3e77f62dcf7953b323f6435481baf46809
Dylan Palmieri <dgpalmieri@gmail.com>
2021-11-19 03:46:38 -0900

    feat: build skeleton for view tests

---
+++ EPR/Library/tests/test_view.cpp
@@ -0,0 +1,11 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
+#include "../view.hpp"
+//#include "view.hpp"
+#include "doctest.h"
+
+TEST_CASE("View Tests"){
+    int a = 1;
+    REQUIRE(a == 1);
+
+}
