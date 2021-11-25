913295d1dbd5f6d574ed5adc515cab6028784a8c
Dylan Palmieri <dgpalmieri@gmail.com>
2021-11-25 12:01:00 -0900

    fix: rename functions

--- EPR/Library/view.cpp
+++ EPR/Library/view.cpp
@@ -9,25 +9,25 @@
 MATRIX viewMatrix = {{1.,0.,0.,0.},{0.,1.,0.,0.},{0.,0.,1.,0.},{0.,0.,0.,1.}};
 
 // inverse of transform to eye ray
-// This gets set only during the inverse() call in setViewMatrix if the 
+// This gets set only during the inverse() call in setViewMatrix if the
 //     matrix is singular (invertable)
 MATRIX viewInverse = {{1.,0.,0.,0.},{0.,1.,0.,0.},{0.,0.,1.,0.},{0.,0.,0.,1.}};
 
-void set_view_matrix(const std::vector<double> & avs_camera) {
+void setViewMatrix(const std::vector<double> & avs_camera) {
     // copy the view matrix over, note that the viewpoint
     // is at (0, 0, 12) facing down the negative Z axis
     for (int i = 0; i < 4; ++i)
     for (int j = 0; j < 4; ++j)
-      viewMatrix[i][j] = avs_camera[i * 4 + j];
+        viewMatrix[i][j] = avs_camera[i * 4 + j];
 
     // invert the matrix so view can be moved instead of moving objects
     if (inverse(viewMatrix, viewInverse) == NON_SINGULAR) {
-    std::cerr << "Camera transformation is non-singular - IGNORING" << std::endl;;
-    MatrixIdentity(viewInverse);
+        std::cerr << "Camera transformation is non-singular - IGNORING" << std::endl;
+        MatrixIdentity(viewInverse);
     }
 }
 
-void output_camera() {
+void outputCamera() {
     std::cerr << "Camera transformation " << std::endl;
     MatrixPrint(viewMatrix);
     std::cerr << "Inverse camera transformation " << std::endl;
@@ -75,6 +75,6 @@ Vector vecInvCameraTransform(const Vector & p) {
     return a;
 }
 
-double camera_lookup(const int & i, const int & j) {
+double cameraLookup(const int & i, const int & j) {
   return viewMatrix[i][j];
 }
