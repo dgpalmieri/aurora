8a5cd59da6c8325d928fad5e9edd210a4f6cd9e4
Dylan Palmieri <dgpalmieri@gmail.com>
2021-11-25 05:20:04 -0900

    feat: add camera_xform tests

--- EPR/Library/tests/test_view.cpp
+++ EPR/Library/tests/test_view.cpp
@@ -4,8 +4,42 @@
 //#include "view.hpp"
 #include "doctest.h"
 
+#include <iostream>
+
 TEST_CASE("View Tests"){
-    int a = 1;
-    REQUIRE(a == 1);
+
+    // set_view_matrix tests
+
+    double zeroMatrix[16] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+
+    set_view_matrix(zeroMatrix);
+    // Is there a way to access the xform and invrse matricies in the view file?
+    // Testing this isn't possible unless we incorporate extra functions
+
+    // camera_xform tests
+    // Because this function is composed entirely of two prints two MatrixPrints (which
+    // are tested in test_rays.cpp), testing this function seems repeetitive and non-DRY
+
+    // inv_camera_xform tests
+
+    Point testPoint = {1, 1, 1};
+    Point retPoint = camera_xform(testPoint);
+
+    REQUIRE(retPoint.x == 0);
+    REQUIRE(retPoint.y == 0);
+    REQUIRE(retPoint.z == 0);
+
+    Point testPoint2 = {2, 2, 2};
+    Point retPoint2 = camera_xform(testPoint2);
+
+    REQUIRE(retPoint2.x == 0);
+    REQUIRE(retPoint2.y == 0);
+    REQUIRE(retPoint2.z == 0);
+
+    // vec_camera_xform tests
+
+    // vec_inv_camera_xform
+
+    // camera_lookup tests
 
 }
