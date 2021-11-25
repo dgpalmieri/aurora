1a2d6b2a6e00a3498f92a97fa1b4ae5b2dd4474b
Dylan Palmieri <dgpalmieri@gmail.com>
2021-11-25 06:30:19 -0900

    feat: change test matrix to be meaningful

--- EPR/Library/tests/test_view.cpp
+++ EPR/Library/tests/test_view.cpp
@@ -10,55 +10,70 @@ TEST_CASE("View Tests"){
 
     // set_view_matrix tests
 
-    double zeroMatrix[16] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+    double testMatrix[16] = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0,
+                             10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0};
 
-    set_view_matrix(zeroMatrix);
+    set_view_matrix(testMatrix);
     // Is there a way to access the xform and invrse matricies in the view file?
     // Ideally, this would be some kind of read-only global variable
 
-    // Now the xform matrix is a 4x4 matrix of zeros, and the inverse matrix is
+    // Now the xform matrix is a 4x4 matrix, and the inverse matrix is
     // still the identity matrix
 
     // camera_xform tests
     // Because this function is composed entirely of two prints two MatrixPrints (which
     // are tested in test_rays.cpp), testing this function seems repeetitive and non-DRY
 
-    // camera_xform tests
+    SUBCASE("camera_xform tests"){
+        Point testPoint = {1, 1, 1};
+        Point retPoint = camera_xform(testPoint);
 
-    Point testPoint = {1, 1, 1};
-    Point retPoint = camera_xform(testPoint);
+        CHECK(retPoint.x == 28);
+        CHECK(retPoint.y == 32);
+        CHECK(retPoint.z == 36);
 
-    REQUIRE(retPoint.x == 0);
-    REQUIRE(retPoint.y == 0);
-    REQUIRE(retPoint.z == 0);
+        Point testPoint2 = {2, 2, 2};
+        Point retPoint2 = camera_xform(testPoint2);
 
-    Point testPoint2 = {2, 2, 2};
-    Point retPoint2 = camera_xform(testPoint2);
+        CHECK(retPoint2.x == 43);
+        CHECK(retPoint2.y == 50);
+        CHECK(retPoint2.z == 57);
+    }
 
-    REQUIRE(retPoint2.x == 0);
-    REQUIRE(retPoint2.y == 0);
-    REQUIRE(retPoint2.z == 0);
+    SUBCASE("inv_camera_xform"){
+        Point testPoint = {1, 1, 1};
+        Point retPoint = camera_xform(testPoint);
 
-    // inv_camera_xform
+        CHECK(retPoint.x == 28);
+        CHECK(retPoint.y == 32);
+        CHECK(retPoint.z == 36);
 
-    Point invTestPoint = {1, 1, 1};
-    Point invRetPoint = camera_xform(testPoint);
+        Point testPoint2 = {2, 2, 2};
+        Point retPoint2 = camera_xform(testPoint2);
 
-    REQUIRE(invRetPoint.x == 1);
-    REQUIRE(invRetPoint.y == 1);
-    REQUIRE(invRetPoint.z == 1);
+        CHECK(retPoint2.x == 43);
+        CHECK(retPoint2.y == 50);
+        CHECK(retPoint2.z == 57);
+    }
 
-    Point invTestPoint2 = {2, 2, 2};
-    Point invRetPoint2 = camera_xform(testPoint2);
+    SUBCASE("vec_camera_xform tests"){
+        Point vecTestPoint = {1, 1, 1};
+        Point vecRetPoint = camera_xform(vecTestPoint);
 
-    REQUIRE(invRetPoint2.x == 2);
-    REQUIRE(invRetPoint2.y == 2);
-    REQUIRE(invRetPoint2.z == 2);
+        CHECK(vecRetPoint.x == 28);
+        CHECK(vecRetPoint.y == 32);
+        CHECK(vecRetPoint.z == 36);
 
-    // vec_camera_xform tests
+        Point vecTestPoint2 = {2, 2, 2};
+        Point vecRetPoint2 = camera_xform(vecTestPoint2);
 
-    // vec_inv_camera_xform
+        CHECK(vecRetPoint2.x == 43);
+        CHECK(vecRetPoint2.y == 50);
+        CHECK(vecRetPoint2.z == 57);
+    }
 
-    // camera_lookup tests
+    SUBCASE("vec_inv_camera_xform"){}
+
+    SUBCASE("camera_lookup tests"){}
 
 }
